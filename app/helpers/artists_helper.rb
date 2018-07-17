95c3c9d7fc61d1fe8b5b3377ee69856dfa84d240
Karl Amoguis <ktamoguis@gmail.com>
2018-07-17 13:06:29 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,11 +3,11 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def author_id_field(post)
-    if post.author.nil?
-      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+  def artist_id_field(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "post[author_id]", post.author_id
+      hidden_field_tag "song[artist_id]", song.artist_id
     end
   end
 
